f3000bc1d47a4ebdc72a26c0d0f24c13ac3d5a36
Sagar Mangulkar <smangul1@binghamton.edu>
2025-05-14 12:15:58 -0500

    First commit - Add Login Authentication

---
+++ JS/authentication.js
@@ -0,0 +1,29 @@
+var userName = document.getElementById("userName");
+var password = document.getElementById("password");
+var signInButton = document.getElementById("signInButton");
+var message = document.getElementById("message");
+
+function clearmessage(){
+    message.innerText = " ";
+}
+signInButton.addEventListener("click",(event) => {
+   // alert("Hello World");
+    event.preventDefault();
+    console.log(userName.value,password.value);
+    if(userName.value === "" || password.value === ""){
+        message.innerText = "Please provide Username and Password";
+    }
+    else if(userName.value === "great" &&  password.value === "learning"){
+        message.style.color = "green";
+        message.innerText = "You have Successfully Logged In";
+        console.log("LoggedIn");
+        //window.location.replace = ("./html/index.html");
+
+        setTimeout(() => {
+            window.location.replace("./html/index.html");
+        },1000)
+    }
+    else{
+        message.innerText = "Username Or Password is Invalid";
+    }
+});
